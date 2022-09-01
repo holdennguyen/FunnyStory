90ba47ff5991900ba76db7e93c8c5f1fd82a9fc6
Hung. Minh Nguyen <89327966+holdennguyen@users.noreply.github.com>
2022-09-01 11:40:34 +0700

    init

---
+++ app/src/main/java/com/example/truyencuoi/M000SplashFrg.java
@@ -0,0 +1,28 @@
+package com.example.truyencuoi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class M000SplashFrg extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        initViews();
+        return inflater.inflate(R.layout.m000_frg_splash, container, false);
+    }
+
+    private void initViews() {
+        new Handler().postDelayed(this::gotoM001Screen, 2000);
+    }
+
+    private void gotoM001Screen() {
+        ((MainActivity) getActivity()).gotoM001Screen();
+    }
+}
